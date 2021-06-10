0c2171b75f8a0ecb079b0efee6f286052ed01eb3
Dzung Nguyen <dzung_min@gmail.com>
2021-06-11 05:29:32 +0700

    config view engine

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const express = require("express")
 const path = require("path")
+const exphbs = require("express-handlebars")
 
 const app = express()
-const publicPath = path.join(__dirname, "../public")
+const port = 3000
 
+// config static folder
+const publicPath = path.join(__dirname, "../public")
 app.use(express.static(publicPath))
 
+// config view engine
+app.engine(".hbs", exphbs({ extname: ".hbs" }))
+app.set("view engine", ".hbs")
+
 app.get("/", (req, res) => {
-  res.send("Hello express")
+  res.render("index", { title: "Weather" })
 })
 
-app.listen(3000, () => {
-  console.log("Server has been started on port 3000")
+app.listen(port, () => {
+  console.log(`Server has been started on port ${port}`)
 })

5595b544eee6b65e58d819e38a58a3b55096f043
Dzung Nguyen <dzung_min@gmail.com>
2021-06-11 06:25:34 +0700

    add handler for '/weather' route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require("express")
 const path = require("path")
 const exphbs = require("express-handlebars")
 
+const getGeoCode = require("./utils/getGeoCode")
+const getWeatherInfo = require("./utils/getWeatherInfo")
+
 const app = express()
 const port = 3000
 
@@ -23,6 +26,26 @@ app.get("/help", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" })
+})
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    res.send({ error: "No location is provided." })
+  } else {
+    ;(async () => {
+      try {
+        const { lat, lon, place_name } = await getGeoCode(req.query.address)
+        const { temperature, feelslike, weather_descriptions } =
+          await getWeatherInfo(lat, lon)
+        res.send({
+          location: place_name,
+          forecast: `${weather_descriptions}. It is ${temperature} degrees out. It feels like ${feelslike} degrees out.`,
+        })
+      } catch (error) {
+        res.send({ error: error.message })
+      }
+    })()
+  }
 })
 
 app.listen(port, () => {
--- src/utils/getWeatherInfo.js
+++ src/utils/getWeatherInfo.js
@@ -2,9 +2,10 @@ const axios = require("axios").default
 
 const getWeatherInfo = async (lat, lon) => {
   try {
-    const url = `http://api.weatherstack.com/current?access_key=b6697f76dd83a79fd3d503adbdfe71b6&query=${lon},${lat}`
+    const url = `http://api.weatherstack.com/current?access_key=b6697f76dd83a79fd3d503adbdfe71b6&query=${lat},${lon}`
     const { data } = await axios.get(url)
-    const { temperature, weather_descriptions, feelslike } = data.current
+    const weather_descriptions = data.current.weather_descriptions[0]
+    const { temperature, feelslike } = data.current
     return { temperature, weather_descriptions, feelslike }
   } catch (error) {
     throw Error("Can not get weather data")

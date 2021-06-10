842b5a547b905c4aaa07d1c4c5bdece75fd449d7
Dzung Nguyen <dzung_min@gmail.com>
2021-06-11 05:20:41 +0700

    config static folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
+const publicPath = path.join(__dirname, "../public")
+
+app.use(express.static(publicPath))
 
 app.get("/", (req, res) => {
   res.send("Hello express")
